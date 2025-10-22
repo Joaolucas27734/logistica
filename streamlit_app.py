6b5f1b7ed9fd2534411fa6050f6c35ca0b0c90b8
Joaolucas27734 <joaolucassportech@gmail.com>
2025-10-22 16:27:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -262,7 +262,7 @@ elif opcao == "🚚 Logística Geral":
         "⚖️ Comparar Variantes"
     ])
 
-    # ======================= TAB 1 ==============================
+ # ======================= TAB 1 ==============================
 with tab1:
     st.subheader("🧾 Pedidos Pagos da Shopify")
 
@@ -331,6 +331,8 @@ with tab1:
 
         # --- Atualizar automaticamente na Shopify ---
         try:
+            import requests
+
             SHOP_NAME = st.secrets["shopify"]["shop_name"]
             ACCESS_TOKEN = st.secrets["shopify"]["access_token"]
             url_base = f"https://{SHOP_NAME}/admin/api/2023-10"
@@ -346,8 +348,6 @@ with tab1:
             if novos_codigos.empty:
                 st.info("Nenhum código de rastreio novo para enviar à Shopify.")
             else:
-                import requests
-
                 for _, row in novos_codigos.iterrows():
                     order_id = str(row["ID"]).replace("#", "").strip()
                     tracking_code = str(row["Codigo de rastreio"]).strip()
@@ -378,6 +378,7 @@ with tab1:
 
         except Exception as e:
             st.error(f"❌ Erro geral ao conectar com a Shopify: {e}")
+
 
 
 # ======================= TAB 2 ==============================
